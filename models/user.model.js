24a45417c1549007ac1f258876ca456e1001ee03
saifmessaoudi <saifmessaoudi99@gmail.com>
2024-02-08 16:27:05 +0100

    model user

--- models/user.model.js
+++ models/user.model.js
@@ -2,8 +2,23 @@ import mongoose from "mongoose";
 
 
 const userSchema = mongoose.Schema(
-    {},
-    { timestamps: true}
+    {
+      firstName : { type: String, required: true},
+      lastName : { type: String, required: true},
+      email : { type: String, required: true, unique: true},
+      username : { type: String, required: true, unique: true},
+      password : { type: String, required: true},
+      role : { type: String, default: "user"},
+      birthDate : { type: Date},
+      bio : { type: String},
+      profilePicture : { type: String , default: "https://louisville.edu/enrollmentmanagement/images/person-icon/image"},
+      friends : [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+      friendRequests : [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+      friendRequestsSent : [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+      favouriteMovies : [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie"}],
+      isBanned : { type: Boolean, default: false},
+      isVerified : { type: Boolean, default: false},
+    }
     ); 
 const User = mongoose.model("User", userSchema);
 
