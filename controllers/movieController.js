8cb16129d3206452030d99d7c231c5665b910709
wassimnsiri <wassim.nsiri@esprit.tn>
2024-02-18 23:51:43 +0100

    remove from favoris done

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -21,3 +21,30 @@ try{
     return res.status(500).json({message: "erreur serveur"})
 }
 };
+export const removefavoris = async (req, res) => {
+    const { username } = req.params;
+    const { movieId } = req.body;
+  
+    try {
+      const user = await User.findOne({ username });
+  
+      if (!user) {
+        return res.status(400).json({ message: "User not found" });
+      }
+  
+      const index = user.favouriteMovies.indexOf(movieId);
+  
+      if (index !== -1) {
+        // Retirer le film de la liste des favoris
+        user.favouriteMovies.splice(index, 1);
+        await user.save();
+        return res.status(200).json({ message: "Movie removed from favorites" });
+      } else {
+        return res.status(400).json({ message: "Movie not found in favorites" });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Server error" });
+    }
+  };
+  
